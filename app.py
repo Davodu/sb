ed6f909704ef42e3c68742bcb30493610c1a8037
Davies Odu <daodu@microsoft.com>
2020-04-27 15:18:06 -0700

    fixing .gitignore and result display bug

--- app.py
+++ app.py
@@ -3,10 +3,8 @@ import os
 import numpy as np
 
 from flask import Flask, request, render_template, send_from_directory
-from utils.model import ImageRetrievalModel
 from utils.util import simple_inference
 import pickle
-
 #run 
 # utils/dataset.py
 # config.py
@@ -17,8 +15,13 @@ import pickle
 #set root directory
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
+
+
+#==================================
 #define model
-model = ImageRetrievalModel(cfg.IMAGE_SIZE)
+#model = ImageRetrievalModel(cfg.IMAGE_SIZE)
+model = None
+#==================================
 
 #get training set vectors
 with open('dataset/modified_new/catalog/hamming_train_vectors.pickle', 'rb') as f:
@@ -27,6 +30,7 @@ with open('dataset/modified_new/catalog/hamming_train_vectors.pickle', 'rb') as 
 #Load training set paths
 with open('dataset/modified_new/catalog/val_paths.pickle', 'rb') as f:
 	train_images_paths = pickle.load(f)
+
 
 #Define Flask app
 app = Flask(__name__, static_url_path='/static')
@@ -39,26 +43,22 @@ def index():
 #Define upload function
 @app.route("/upload", methods=["POST"])
 def upload():
-
-	upload_dir = os.path.join(APP_ROOT, "uploads/")
-
-	if not os.path.isdir(upload_dir):
-		os.mkdir(upload_dir)
-
-	for img in request.files.getlist("file"):
-		img_name = img.filename
-		destination = "/".join([upload_dir, img_name])
-		img.save(destination)
-
-	#inference
-	result = np.array(train_images_paths)[simple_inference(model, train_vectors, os.path.join(upload_dir, img_name), cfg.IMAGE_SIZE[0])]
-
-	result_final = []
-
-	for img in result:
-		result_final.append("images/"+img.split("/")[-1]) #example: dataset/train/img1.png -> [dataset, train, img1.png] -> [-1] = img.png
-
-	return render_template("result.html", image_name=img_name, result_paths=result_final[:-2]) #added [:-2] just to have equal number of images in the result page per row
+    upload_dir = os.path.join(APP_ROOT, "uploads/")
+    if not os.path.isdir(upload_dir):
+        os.mkdir(upload_dir)
+    for img in request.files.getlist("file"):
+        img_name = img.filename
+        destination = "/".join([upload_dir, img_name])
+        img.save(destination)
+	#inference fetches 20 images
+    result = np.array(train_images_paths)[simple_inference(model, train_vectors, os.path.join(upload_dir, img_name), cfg.IMAGE_SIZE[0])]
+    #get original result path from symlink
+    result = [os.readlink(sym_path[3:]) for sym_path in result] 
+    
+    result_final = []
+    for img in result:
+        result_final.append("Img/img/"+ ('/').join(img.split("/")[-2:])) 
+    return render_template("result.html", image_name=img_name, result_paths=result_final[:-8])#display only 12 images in user interface
 
 #Define helper function for finding image paths
 @app.route("/upload/<filename>")
--- utils/util.py
+++ utils/util.py
@@ -2,7 +2,9 @@ import numpy as np
 from scipy.spatial.distance import hamming, cosine
 from keras.preprocessing import image
 from keras.applications.vgg16 import preprocess_input
+from keras.applications.vgg16 import VGG16
 
+import config as cfg 
 
 def hamming_distance(training_set_vectors, query_vector, top_n=20):
     '''
@@ -59,6 +61,33 @@ def simple_inference(model,
     # ImageNet dataset
     img = np.expand_dims(img, axis=0)
     img = preprocess_input(img)
+    
+    model = VGG16(input_shape=cfg.IMAGE_SIZE + [3], weights='imagenet', include_top=False)
+
+    # don't train existing weights
+    for layer in model.layers:
+        layer.trainable = False
+    
+    #add more layers in future
+    
+    #x = Flatten()(vgg.output)
+    #x = Dense(4096, activation='relu', )(x)
+    #x = Dropout(0.5)(x)
+    #prediction = Dense(6, activation='softmax')(x)
+    
+    # create a model object
+    #model = Model(inputs=vgg.input, outputs=prediction)
+    
+    # specify cost and optimization method to use
+        
+    
+    model.compile(
+        loss='categorical_crossentropy',
+        optimizer='rmsprop',
+        metrics=['accuracy']
+        )
+    model._make_predict_function()
+    
     
     features = model.predict([img])
     features_flatten = features.reshape(3*3*512)
